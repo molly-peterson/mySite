0757167b33551c581a53756c9afeefef07f9f123
molly-peterson <51032532+molly-peterson@users.noreply.github.com>
2020-11-12 22:11:36 -0500

    Add files via upload

--- A6/js/mult_table.js
+++ A6/js/mult_table.js
@@ -8,100 +8,128 @@
 */
 
 // variables for holding form data
-var c_Start, c_End, r_Start, r_End;
-c_Start = $("#cStart");
-r_Start = $("#rStart");
-c_End = $("#cEnd");
-r_End = $("#rEnd");
+var cStart, cEnd, rStart, rEnd;
+cStart = $("#cStart");
+rStart = $("#rStart");
+errortab = $("#error-tab");
+
 /*
     The getInput function takes no arguments and has no returns. This function
     is called by the click of the submit button on the form and takes in the
     numbers submitted on that form and stores them in variables. The error
     handling and drawing table functions are also called from here.
 */
-$().submit(function() {
+
+$().ready(function() {
+
     // select the form for validation by name attribute
     $("#input-form").validate({
         rules: {
+
             // set requirements for column start input
             cStart: {
                 required: true,
                 range: [-50,50],
-                digit: true
+                digit: true,
+                max: 50
             },
+
             // set requirements for column end input
             cEnd: {
                 required: true,
                 range: [-50,50],
                 digit: true,
-                max: parseInt(c_Start.value, 10)
+                max: 50,
+                max_greater: parseInt(cStart.value, 10)
             },
+
             // set requirements for row start input
             rStart: {
                 required: true,
                 range: [-50,50],
-                digit: true
+                digit: true,
+                max: 50
             },
+
             // set requirements for row end input
             rEnd: {
                 required: true,
                 range: [-50,50],
                 digit: true,
-                max: parseInt(r_Start.value, 10)
+                max: 50,
+                max_greater: parseInt(rStart.value, 10)
             }
         },
-
 
         errorElement: "div",
         errorPlacement: function(error, element) {
                 element.after(error);
+				error.css({position: "absolute"});
         },
 
         messages: {
+
             cStart: {
                 required: "A column start value is required",
                 digit: "The value of column start input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
+
             cEnd: {
                 required: "A column end value is required",
                 digit: "The value of column end input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
+
             rStart: {
                 required:  "A row start value is required",
                 digit: "The value of row start input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
+
             rEnd: {
                 required: "A row end value is required",
                 digit: "The value of row end input must be an integer",
                 range: "You must input a digit between -50 and 50"
             }
+        },
+
+        submitHandler: function(form) {
+            form.submit();
         }
+
     })
+
 })
 
+
+
 // function for validation of max greater than min
-jQuery.validator.addMethod("max", function(value, element, param) {
+
+jQuery.validator.addMethod("max_greater", function(value, element, param) {
     value = parseInt(value.value, 10);
     return this.optional(element) || value > param;
 }, "The max value must be greater than the min value");
 
+
+
 function getInput() {
+
     // read in values from form, form checks for number input on its own
-    c_Start = parseInt(document.getElementById('cStart').value, 10);
-    c_End = parseInt(document.getElementById('cEnd').value, 10);
-    r_Start = parseInt(document.getElementById('rStart').value, 10);
-    r_End = parseInt(document.getElementById('rEnd').value, 10);
+    cStart = parseInt(document.getElementById('cStart').value, 10);
+    cEnd = parseInt(document.getElementById('cEnd').value, 10);
+    rStart = parseInt(document.getElementById('rStart').value, 10);
+    rEnd = parseInt(document.getElementById('rEnd').value, 10);
 
     drawTable();
 }
+
 /*
     The drawTable function does not take any arguments and has no returns. This
     function creates the table to display on the page with the multiplication
     data.
 */
+
 function drawTable() {
     var tbl = "<table class=\"table-striped\">";
 
