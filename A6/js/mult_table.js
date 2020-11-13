c604de6d3afdf2740b66ad861d98829ef57425f1
molly-peterson <51032532+molly-peterson@users.noreply.github.com>
2020-11-12 23:41:36 -0500

    Fixed addmethod

--- A6/js/mult_table.js
+++ A6/js/mult_table.js
@@ -2,15 +2,17 @@
     Molly Peterson, molly_peterson@student.uml.edu
     University of Massachusetts Lowell
     Computer Science, COMP 4610 GUI I
-    Task: Creating an Interactive Dynamic Table
-    File: github.com/molly-peterson/mySite/A5/js/mult_table.js
-    created: Oct 28, 2020 @ 8:00 PM
+    Task: Error Handling with jQuery Validator
+    File: github.com/molly-peterson/mySite/A6/js/mult_table.js
+    created: Nov 12, 2020 @ 11:00 PM
 */
 
+
+
 // variables for holding form data
-var c_Start, c_End, r_Start, r_End;
-c_Start = $("#cStart");
-r_Start = $("#rStart");
+var cStart, cEnd, rStart, rEnd;
+cStart = $("#cStart");
+rStart = $("#rStart");
 errortab = $("#error-tab");
 
 /*
@@ -23,46 +25,45 @@ errortab = $("#error-tab");
 $().ready(function() {
 
     // select the form for validation by name attribute
+
     $("#input-form").validate({
+
         rules: {
 
             // set requirements for column start input
             cStart: {
                 required: true,
-                min: -50,
+                range: [-50,50],
                 digit: true,
-                max: 50,
-		max_greater: false
+				        max: false
             },
 
             // set requirements for column end input
             cEnd: {
                 required: true,
-                min: -50,
+                range: [-50,50],
                 digit: true,
-                max: 50,
-                max_greater: cStart
+                max: cStart
             },
 
             // set requirements for row start input
             rStart: {
                 required: true,
-                min: -50,
+                range: [-50,50],
                 digit: true,
-                max: 50,
-		max_greater: false
+				        max: false
             },
 
             // set requirements for row end input
             rEnd: {
                 required: true,
-                min: -50,
+                range: [-50,50],
                 digit: true,
-                max: 50,
-                max_greater: rStart
+                max: rStart
             }
         },
 
+        // move the error message placement
         errorElement: "div",
         errorPlacement: function(error, element) {
                 element.after(error);
@@ -70,25 +71,25 @@ $().ready(function() {
         },
 
         messages: {
-
+            // custom error messages for column start
             cStart: {
                 required: "A column start value is required",
                 digit: "The value of column start input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
-
+            // custom error messages for column end
             cEnd: {
                 required: "A column end value is required",
                 digit: "The value of column end input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
-
+            // custom error messages for row start
             rStart: {
                 required:  "A row start value is required",
                 digit: "The value of row start input must be an integer",
                 range: "You must input a digit between -50 and 50"
             },
-
+            // custom error messages for row end
             rEnd: {
                 required: "A row end value is required",
                 digit: "The value of row end input must be an integer",
@@ -99,28 +100,21 @@ $().ready(function() {
         submitHandler: function(form) {
             form.submit();
         }
-
     })
-
 })
 
-
-
 // function for validation of max greater than min
-
-jQuery.validator.addMethod("max_greater", function(value, element, param) {
-    return this.optional(element) || value > param;
+jQuery.validator.addMethod("max", function(value, element, param) {
+return +$(element).val() > +$(param[0]).val();
 }, "The max value must be greater than the min value");
-
-
 
 function getInput() {
 
     // read in values from form, form checks for number input on its own
-    c_Start = parseInt(document.getElementById('cStart').value, 10);
-    c_End = parseInt(document.getElementById('cEnd').value, 10);
-    r_Start = parseInt(document.getElementById('rStart').value, 10);
-    r_End = parseInt(document.getElementById('rEnd').value, 10);
+    cStart = parseInt(document.getElementById('cStart').value, 10);
+    cEnd = parseInt(document.getElementById('cEnd').value, 10);
+    rStart = parseInt(document.getElementById('rStart').value, 10);
+    rEnd = parseInt(document.getElementById('rEnd').value, 10);
 
     drawTable();
 }
@@ -135,19 +129,19 @@ function drawTable() {
     var tbl = "<table class=\"table-striped\">";
 
     // create a new row on each iteration of outer loop
-    for(var i= (r_Start-1); i <= r_End; i++) {
+    for(var i= (rStart-1); i <= rEnd; i++) {
         tbl += "<tr>";
 
         // fill in each row on each iteration of inner loop
-        for(var j= (c_Start-1); j <= c_End; j++) {
+        for(var j= (cStart-1); j <= cEnd; j++) {
 
-            if((i == (r_Start-1)) && (j == (c_Start-1))) {
+            if((i == (rStart-1)) && (j == (cStart-1))) {
                 // add an empty cell in top left
                 tbl += "<td></td>";
-            }else if((i == (r_Start-1)) && ((j > (c_Start-1)) || (j <= c_End))) {
+            }else if((i == (rStart-1)) && ((j > (cStart-1)) || (j <= cEnd))) {
                 // add the column range
                 tbl += "<td id=\"tbl_h\">" + j + "</td>";
-            } else if((j == (c_Start-1)) && ((i > (r_Start-1)) || (i <= r_End))) {
+            } else if((j == (cStart-1)) && ((i > (rStart-1)) || (i <= rEnd))) {
                 // add the rows range
                 tbl += "<td id=\"tbl_h\">" + i + "</td>";
             } else {
